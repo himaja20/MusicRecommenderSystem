f494164bb550e039072b1bee8cf0296237ef8244
Himaja Rachakonda <hr970@nyu.edu>
2015-12-05 01:35:57 -0500

    adding code for computing loudness average

--- code/generateAvg.py
+++ code/generateAvg.py
@@ -11,14 +11,19 @@ def process_tracks():
 def load_track(line):	
 	t = {}
 	f = line.split('\t')
+	loudness = f[1].split(',')
 	seg_start = f[2].split(',')
 	seg_pitches = f[3].split(',')
 	seg_timbre = f[4].split(',')
 	track_id = f[0]
 	COF_COUNT = 12
 	numOfSegments = len(seg_start)
+	
 	sumPitch = [0 for s1 in range(12)]
 	sumTimbre = [0 for s2 in range(12)]
+	sumLoudness = 0
+
+	avgLoudness = 0
 	avgTimbre = [0 for a1 in range(12)]
 	avgPitch = [0 for a2 in range(12)]
 	for i in range(0,numOfSegments):
@@ -34,6 +39,11 @@ def load_track(line):
 	avgStrPitch = ''
 	avgStrTimbre = ''
 	i = 0
+	
+	for val in range(len(loudness)):
+		sumLoudness = sumLoudness + float(loudness[val])
+	avgLoudness = sumLoudness / numOfSegments
+
 	for av in range(len(avgPitch)):
 		if (i <= 10):
 			print i
@@ -49,6 +59,9 @@ def load_track(line):
 	with open("timbreAvg",'a') as f2:
 		f2.write('{0},{1}'.format(track_id,avgStrTimbre))
 		f2.write('\n')
+	with open("loudnessAvg",'a') as f3:
+		f3.write('{0},{1}'.format(track_id,avgLoudness))
+		f3.write('\n')
 
 if __name__ == '__main__':
     process_tracks()
